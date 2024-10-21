ad1cd808214d73fb0f4d8d681411373229cb06cd
opilane <opilane@HP-CZC3217070>
2024-10-21 16:32:00 +0300

    Added SigmaImageViewModel and did some stuff in _Images

---
+++ Looksmaxxing/Models/Sigmas/SigmaImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Looksmaxxing.Models.Sigmas
+{
+    public class SigmaImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public string ImageTitle { get; set; }
+        public byte[] ImageData { get; set; }
+        public string Image {  get; set; }
+        public Guid? SigmaID { get; set; }
+    }
+}
