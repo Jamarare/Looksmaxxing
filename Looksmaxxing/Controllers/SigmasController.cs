d0ef3a207d9adc64906fc7f0e8cc9b6586fcf0ce
opilane <opilane@ORDI-402605>
2024-10-24 14:16:24 +0300

    made some changes(alot)

--- Looksmaxxing/Controllers/SigmasController.cs
+++ Looksmaxxing/Controllers/SigmasController.cs
@@ -3,6 +3,7 @@ using Looksmaxxing.Core.ServiceInterface;
 using Looksmaxxing.Models.Sigmas;
 using Microsoft.AspNetCore.Mvc;
 using Looksmaxxing.ApplicationServices.Services;
+using Looksmaxxing.Core.Dto;
 
 namespace Looksmaxxing.Controllers
 {
@@ -34,6 +35,49 @@ namespace Looksmaxxing.Controllers
                     SigmaLevel = x.SigmaLevel,
                 });
             return View(resultingInventory);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            SigmaCreateViewModel vm = new();
+            return View("Create",vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(SigmaCreateViewModel vm)
+        {
+            var dto = new SigmaDto()
+            {
+                SigmaName = vm.SigmaName,
+                SigmaXP = 0,
+                SigmaXPNextLevel = 100,
+                SigmaLevel = 0,
+                SigmaType = (Core.Dto.SigmaType)vm.SigmaType,
+                SigmaStatus = (Core.Dto.SigmaStatus)vm.SigmaStatus,
+                SigmaMove = vm.SigmaMove,
+                SigmaMovePower = vm.SigmaMovePower,
+                SpecialSigmaMove = vm.SpecialSigmaMove,
+                SpecialSigmaMovePower = vm.SpecialSigmaMovePower,
+                SigmaWasBorn = vm.SigmaWasBorn,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Files = vm.Files,
+                Image = vm.Image,
+                .Select(x => new FileToDatabaseDto
+                 {
+                    ID = x.ImageID,
+                    ImageData = x.ImageData,
+                    ImageTitle = x.ImageTitle,
+                    SigmaID = x.ImageID,
+                 }).ToArray()
+            };
+            var result = await _sigmasServices.Create(dto);
+
+            if (result != null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Looksmaxxing/Models/Sigmas/SigmaCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Looksmaxxing.Models.Sigmas
+{
+
+    public class SigmaCreateViewModel
+        {
+            public Guid Id { get; set; }
+
+            public string SigmaName { get; set; }
+            public int SigmaXP { get; set; }
+            public int SigmaXPNextLevel { get; set; }
+            public int SigmaLevel { get; set; }
+            public SigmaType SigmaType { get; set; }
+            public SigmaStatus SigmaStatus { get; set; }
+            public int SigmaMovePower { get; set; }
+            public int SigmaMove { get; set; }
+            public int SpecialSigmaMovePower { get; set; }
+            public int SpecialSigmaMove { get; set; }
+            public DateTime SigmaWasBorn { get; set; }
+            public DateTime SigmaDied { get; set; }
+
+            
+            public List<IFormFile> Files { get; set; }
+            public List<SigmaImageViewModel> Images { get; set; } = new List<SigmaImageViewModel>();
+
+            //db only
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
+        }   
+}
