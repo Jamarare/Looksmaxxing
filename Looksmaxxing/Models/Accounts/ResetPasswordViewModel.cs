4ad4c13409250222d9732d6e5a0a73b37d8a539a
opilane <opilane@HP-CZC3217070>
2024-11-20 14:10:59 +0200

    Added accountDto and preperation for the controller and view

---
+++ Looksmaxxing/Models/Accounts/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Looksmaxxing.Models.Accounts
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm your password by typing it again: ")]
+        [Compare("Password", ErrorMessage = "Password and its cofirtmation do not match. Please try again. ")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
