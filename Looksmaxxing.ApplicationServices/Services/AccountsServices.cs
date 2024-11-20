bfb50070b8201e52d45e011b5457912c49656ba2
opilane <opilane@HP-CZC3217070>
2024-11-20 15:20:45 +0200

    Accounts interface with service

---
+++ Looksmaxxing.ApplicationServices/Services/AccountsServices.cs
@@ -0,0 +1,26 @@
+﻿using Looksmaxxing.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Looksmaxxing.ApplicationServices.Services
+{
+    public class AccountsServices : IAccountsServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountsServices
+            (
+                UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
