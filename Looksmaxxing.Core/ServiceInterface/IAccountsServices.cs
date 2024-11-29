e041ad802b810c8455577381120e76910836bc34
opilane <opilane@HP-CZC3217070>
2024-11-29 11:25:33 +0200

    Added missing logindto added methods for logging in and registering and interface

--- Looksmaxxing.Core/ServiceInterface/IAccountsServices.cs
+++ Looksmaxxing.Core/ServiceInterface/IAccountsServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Looksmaxxing.Core.Domain;
+using Looksmaxxing.Core.Dto.AccountsDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace Looksmaxxing.Core.ServiceInterface
 {
     public interface IAccountsServices
     {
+        Task<ApplicationUser> ConfirmEmail(string userId, string token);
 
+        Task<ApplicationUser> Register(ApplicationUserDto dto);
+
+        Task<ApplicationUser> Login(LoginDto dto);
     }
 }
