20b81b99bf8e1f14c0b7374ac77178c1cca51c37
opilane <opilane@HP-CZC321706B>
2024-12-04 13:55:49 +0200

    .

--- Looksmaxxing/Program.cs
+++ Looksmaxxing/Program.cs
@@ -1,6 +1,9 @@
 using Looksmaxxing.ApplicationServices.Services;
+using Looksmaxxing.Core.Domain;
 using Looksmaxxing.Core.ServiceInterface;
 using Looksmaxxing.Data;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +12,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ISigmasServices, SigmasServices>();
 builder.Services.AddScoped<IFileServices, FileServices>();
+builder.Services.AddScoped<IAccountsServices, AccountsServices>();
 
 builder.Services.AddDbContext<LooksmaxxingContext>(
     Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequiredLength = 3;
+    options.Tokens.EmailConfirmationTokenProvider = "CustomEmailConfirmation";
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+})
+    .AddEntityFrameworkStores<LooksmaxxingContext>()
+    .AddDefaultTokenProviders()
+    .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>("CustomEmailConfirmation")
+    .AddDefaultUI();
+//all tokenss
+builder.Services.Configure<DataProtectionTokenProviderOptions>(
+    options => options.TokenLifespan = TimeSpan.FromHours(5)
+    );
+//email tokens confirmation
+builder.Services.Configure<CustomEmailConfirmationTokenProviderOptions>(
+    options => options.TokenLifespan = TimeSpan.FromDays(3)
+    );
 
 var app = builder.Build();
 
