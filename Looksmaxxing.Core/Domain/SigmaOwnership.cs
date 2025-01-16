3fbbd1fb4b418b460a56e2d07efde49c4dc1313e
opilane <opilane@HP-CZC321706P>
2025-01-16 13:26:38 +0200

    smth came

---
+++ Looksmaxxing.Core/Domain/SigmaOwnership.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Looksmaxxing.Core.Domain
+{
+    public class SigmaOwnership : Sigma
+    {
+        public Guid OwnershipID { get; set; }
+        public int SigmaHealth { get; set; }
+        public int SigmaXP { get; set; }
+        public int SigmaXPNextLevel { get; set; }
+        public int SigmaLevel { get; set; }
+        public SigmaStatus SigmaStatus { get; set; }
+        public int SigmaMovePower { get; set; }
+        public int SpecialSigmaMovePower { get; set; }
+        public DateTime SigmaWasBorn { get; set; }
+        public DateTime SigmaDied { get; set; }
+        //public string OwnedByPlayerProfile { get; set; } //is string but holds guid
+        //db only
+        public DateTime OwnershipCreatedAt { get; set; }
+        public DateTime OwnershipUpdatedAt { get; set; }
+    }
+}
---
+++ Looksmaxxing.Core/Dto/AccountsDtos/PlayerProfileDto.cs
@@ -0,0 +1,27 @@
+﻿using Looksmaxxing.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Looksmaxxing.Core.Dto.AccountsDtos
+{
+    public class PlayerProfileDto
+    {
+        public Guid ID { get; set; }
+        public Guid ApplicationUserID { get; set; } // 1-1
+        public string ScreenName { get; set; }
+        public int Credits { get; set; }
+        public List<SigmaOwnership> MySigmas { get; set; }
+        public int Victories { get; set; }
+        public int MyProperty { get; set; }
+        public ProfileStatus CurrentStatus { get; set; }
+        public bool ProfileType { get; set; } //true, admin, false, player
+        //dbonly
+        public DateTime ProfileCreatedAt { get; set; }
+        public DateTime ProfileModifiedAt { get; set; }
+        public DateTime ProfileAttributedToAnAccountUserAt { get; set; }
+        public DateTime ProfileStatusLastChangedAt { get; set; }
+    }
+}
